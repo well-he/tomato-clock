f9b1bd9d938a190c07e80d5a4ceea769eeaf96b4
showell <hsw_0423@163.com>
2021-01-31 01:15:06 +0800

    完成，UI比较简陋

--- main.js
+++ main.js
@@ -2,21 +2,22 @@ const {
     app,
     dialog,
     BrowserWindow,
+    Notification,
     ipcMain
 } = require('electron')
 
 let win;
 app.on('ready', () => {
     win = new BrowserWindow({
-        width: 550,
-        height: 600,
+        width: 400,
+        height: 500,
         resizable: false,
         webPreferences: {
             nodeIntegration: true
         },
         maximizable: false,
-        menuBarVisible: false,
     })
+    win.setMenuBarVisibility(false)
     win.loadFile('./index.html')
     // win.loadFile('./demo.html')
 
@@ -41,4 +42,20 @@ function handleIPC() {
         })
         return res;
     });
+    ipcMain.on('rest-notification', function () {
+        setTimeout(() => {
+            const notification = new Notification({
+                title: '休息结束',
+                body: '休息结束，请开始工作'
+            })
+            notification.show();
+        }, 5 * 1000);
+    })
+    ipcMain.on('end-work', function () {
+        const notification = new Notification({
+            title: '结束',
+            body: '计时已经结束'
+        })
+        notification.show();
+    })
 }
--- render/renderer.js
+++ render/renderer.js
@@ -2,100 +2,127 @@ const {
     ipcRenderer
 } = require('electron');
 
-const Timer = require('timer.js')
+const Timer = require('timer.js');
 
-let workTimer = new Timer({
-    // ontick: (ms) => {
-    //     // updateTime(ms);
-    // },
-    onend: () => {
-        notification();
-    },
-    onstop: () => {
-        // alert('停止')
-    }
-    // onpause: () => {
-    //     console.log('pause');
-    // }
-})
+let timeCanvas;
+let ctx;
 
 new Vue({
     el: '#app',
     data: function () {
         return {
             times: [{
-                value: 5 * 60,
-                label: '5分钟'
-            }, {
-                value: 10 * 60,
-                label: '10分钟'
-            }, {
-                value: 30 * 60,
-                label: '30分钟'
-            }, {
-                value: 60 * 60,
-                label: '60分钟'
-            }, {
-                value: 90 * 60,
-                label: '90分钟'
-            }, {
-                value: 120 * 60,
-                label: '120分钟'
-            }],
-            during: ''
+                    value: 5,
+                    label: 'test'
+                },
+                {
+                    value: 5 * 60,
+                    label: '5分钟'
+                }, {
+                    value: 10 * 60,
+                    label: '10分钟'
+                }, {
+                    value: 30 * 60,
+                    label: '30分钟'
+                }, {
+                    value: 60 * 60,
+                    label: '60分钟'
+                }, {
+                    value: 90 * 60,
+                    label: '90分钟'
+                }, {
+                    value: 120 * 60,
+                    label: '120分钟'
+                }
+            ],
+            workTimer: new Timer(),
+            during: '',
         }
     },
     methods: {
-        workStart() {
-            workTimer.start(this.during)
-            executeClock(this.during)
+        //初始化canvas
+        initCanvas() {
+            timeCanvas = document.getElementById('clockCanvas');
+            ctx = timeCanvas.getContext('2d');
         },
-        endstart() {
-            clearInterval(workTimer.timer);
-            workTimer.stop();
-            this.during = 0;
-            this.$notify({
-                title: '已结束',
-                message: '计时停止'
+        //初始化Timer
+        initTimer() {
+            // timer开始事件
+            this.workTimer.on('start', e => {
+                console.log('start' + e + 'ms');
+                this.executeClock(this.during)
             });
-        }
+            // timer更新事件
+            this.workTimer.on('tick', (e) => {});
+            // timer停止事件
+            this.workTimer.on('stop', e => {
+                ipcRenderer.send('end-work')
+            });
+            // timer暂停事件
+            this.workTimer.on('pause', e => {});
+            // timer结束事件
+            this.workTimer.on('end', e => {
+                this.notification();
+                this.workEnd();
+            });
+        },
+
+        workStart() {
+            //开始工作 调用start实际时调用timer的开始事件
+            this.workTimer.start(this.during)
+        },
+
+        workEnd() {
+            //工作结束事件
+            clearInterval(this.workTimer.timer); //清理计时器
+            this.workTimer.stop(); //停止timer
+            this.during = 0; //设置时间为0
+            this.clearCanvas(); //清除canvas
+        },
+
+        drawClock(time) {
+            ctx.font = "60px Arial";
+            ctx.textAlign = 'center';
+            ctx.strokeText(time + 's', 100, 100);
+        },
+
+        executeClock(time) {
+            if (this.workTimer.timer) clearInterval(this.workTimer.timer)
+            this.workTimer.timer = setInterval(() => {
+                this.clearCanvas();
+                this.drawClock(time);
+                time--;
+                if (time < 0) {
+                    clearInterval(this.workTimer.timer);
+                }
+            }, 1000);
+        },
+
+        async notification() {
+            let res = await ipcRenderer.invoke('work-notification');
+            if (res === "rest") {
+                this.$message({
+                    type: 'info',
+                    duration: '1000',
+                    message: '5分钟后将会提醒您!'
+                });
+                ipcRenderer.send('rest-notification')
+            } else if (res === "work") {
+                this.$message({
+                    type: 'info',
+                    duration: '1000',
+                    message: '请重新设置时间！'
+                });
+            }
+        },
+
+        clearCanvas() {
+            timeCanvas.setAttribute('width', 200);
+        },
+
+    },
+    mounted() {
+        this.initCanvas();
+        this.initTimer();
     }
 })
-
-const timeCanvas = document.querySelector('#clockCanvas');
-
-function drawClock(time) {
-    /**
-     * @type {HTMLCanvasElement}
-     */
-
-    const ctx = timeCanvas.getContext('2d');
-    ctx.font = "60px Arial";
-    ctx.textAlign = 'center';
-    ctx.strokeText(time + 's', 100, 100);
-
-}
-
-function executeClock(time) {
-    if (workTimer.timer) clearInterval(workTimer.timer)
-    workTimer.timer = setInterval(() => {
-        timeCanvas.setAttribute('width', 200);
-        drawClock(time);
-        time--;
-        if (time < 0) {
-            clearInterval(workTimer.timer);
-        }
-    }, 1000);
-}
-
-async function notification() {
-    let res = await ipcRenderer.invoke('work-notification');
-    if (res === "rest") {
-        console.log(res);
-        setTimeout(() => {
-            alert('休息结束')
-        }, 5 * 1000);
-    } else if (res === "work") {
-        workTimer.start(10);
-    }
-}
